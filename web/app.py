1b117eea63ccfc40f70b147e934b23aeb976c440
AaronNazareth <jevil257@gmail.com>
2024-01-20 14:56:31 +0530

    fix:added streamlit_webrtc

--- web/app.py
+++ web/app.py
@@ -3,8 +3,11 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import tensorflow as tf
-import time
 from keras.models import model_from_json
+from streamlit_webrtc import webrtc_streamer,WebRtcMode
+from collections import deque
+from typing import List
+import queue
 
 #load model
 model = model_from_json(open("./output/model.json", "r").read())
@@ -46,6 +49,21 @@ def main():
     st.title("Real-time Emotion Recognition")
     st.write("Upload an image or use your webcam to detect emotions.")
 
+    # Define the video processor class
+    # this class is used to process the video frames in real time
+    class VideoProcessor:
+        result_queue: queue.Queue = queue.Queue()
+        def recv(self, frame):
+            # Convert the frame to numpy array
+            frame_array = np.array(frame.to_ndarray(format="bgr24"))
+            # Predict the emotion
+            predicted_emotion = predict_emotion(frame_array)
+            print(predicted_emotion)
+            # Update the text in the placeholder
+            self.result_queue.put(predicted_emotion)
+            # Send the predicted emotion to the Streamlit UI
+            return frame
+    
     # Choose the input source
     input_source = st.radio("Select input source:", ("Upload Image", "Webcam"))
 
@@ -66,38 +84,39 @@ def main():
 
     elif input_source == "Webcam":
         # Open the webcam
-        cap = cv2.VideoCapture(0)
-        st.text("Please note emotion is predicted every 3 seconds.")
-        # Create a placeholder for the image
-        image_placeholder = st.empty()
-        # Create a placeholder for the text
-        text_placeholder = st.empty()
-        # Read and display frames from the webcam
-        counter = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                continue
-            # Update the image in the placeholder
-            image_placeholder.image(frame, channels="BGR", use_column_width=True)
-            # Convert the frame to numpy array
-            frame_array = np.array(frame)
-            
-            # Predict the emotion every 3 seconds
-            if counter % 30 == 0:
-                # Predict the emotion
-                predicted_emotion = predict_emotion(frame_array)
-                # Update the text in the placeholder
-                text_placeholder.text("Predicted Emotion: " + predicted_emotion)
-
-            counter += 1
-            time.sleep(0.1)
-            # Break the loop if 'q' is pressed
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        # Release the webcam
-        cap.release()
+        labels_placeholder = st.empty() 
+        # we use webrtc_streamer to capture video frames from webcam 
+        #  if we use opencv to capture video frames, we cannot use the 
+        #  get user video only server video
+        webrtc_ctx = webrtc_streamer(
+            key="example",
+            mode=WebRtcMode.SENDRECV,
+             video_processor_factory=VideoProcessor,
+            rtc_configuration={  # Add this line
+                "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+            },
+            media_stream_constraints={"video": True, "audio": True},
+        )
+        st.text("Please note emotion is predicted every second.")
+        if st.checkbox("Show the detected labels", value=True):
+            if webrtc_ctx.state.playing: 
+                # NOTE: The video transformation with object detection and 
+                # this loop displaying the result labels are running 
+                # in different threads asynchronously. 
+                # Then the rendered video frames and the labels displayed here 
+                # are not strictly synchronized. 
+                while True: 
+                    if webrtc_ctx.video_processor.result_queue: 
+                        try: 
+                            print(webrtc_ctx.video_processor.result_queue.qsize())
+                            result = webrtc_ctx.video_processor.result_queue.get( 
+                                timeout=1.0 
+                            ) 
+                        except queue.Empty: 
+                            result = None 
+                        labels_placeholder.markdown('<h3>Predicted Emotion: '+ result +'<h3>', unsafe_allow_html=True) 
+                    else: 
+                        break 
 
 if __name__ == '__main__':
     main()
